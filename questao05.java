3dccc1861afb94fc1360bb7d4e81d7f5b750f0a4
Taticalzero <52974709+Taticalzero@users.noreply.github.com>
2020-02-13 11:43:37 -0300

    Update questao05.java

--- questao05.java
+++ questao05.java
@@ -1,11 +1,16 @@
-import  java.util.Scanner;
+/*
+João deseja fazer um sistema que que solicite para a pessoa digitar um número qualquer,
+posteriormente, realize a soma de todos os algarismos deste mesmo número
+*/
+
+import  java.util.Scanner; // Biblioteca Scanner
 
 public class questao05{
 
     public static void main(String[] arg){
-        System.out.println("Informe um numero: ");
+        System.out.println("Informe um numero: "); //Solicitando numero ao usuario
         int num = new Scanner(System.in).nextInt();
-        int soma = 0;
+        int soma = 0; // Varialvel responsavel para somar os algarismos separados . EX 12 = 1 + 2 = 3
 
         while(num>0){
             // Obtenhe-se o modulo do numero antes de se realizar a soma
@@ -14,7 +19,7 @@ public class questao05{
              soma += modulo;
              // divide-se por fim por 10 para chegar no novo valor
              num/=10;
-             // procedimento sendo printado passo a passo
+             // procedimento sendo printado passo a passo na mudança 
              System.out.println("Valor de soma é  ", +soma);
         }
         System.out.println(); // salto de linha
