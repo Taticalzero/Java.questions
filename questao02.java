7303acbb7a186028c8730f3ff7e82f4f217af9a3
Taticalzero <52974709+Taticalzero@users.noreply.github.com>
2020-02-13 11:04:48 -0300

    Update questao02.java

--- questao02.java
+++ questao02.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
+/*
+Desenvolva um sistema de login e senha. Solicitando ao usuário o seu login e armazene a
+informação em uma variável. Posteriormente a sua senha e coloque-a em outra variável. Por
+fim, verifique se o login e a senha são iguais àquele definido por você.
+*/
+
+
+import java.util.Scanner; // Biblioteca scanner
     public class questao02{
 
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
             //Login
-            System.out.println("Login> ");
+            System.out.println("Login: ");
             String login = in.nextLine();
             //Senha
-            System.out.println("Senha> ");
+            System.out.println("Senha: ");
             String senha = in.nextLine();
-    
+            System.out.println(""); // saltar linha
+               // Autenticação do usuario e senha por string 
             if(login.equals("arata") && senha.equals("123-9658") ){
                 System.out.printf("Usuario %s efetuo login", login);
             }else{
