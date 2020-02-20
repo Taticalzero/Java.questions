2490321158db58ecb1d4be4a3a69f9cbe0a451fd
Tatticalzero <52974709+Taticalzero@users.noreply.github.com>
2020-02-20 14:04:59 -0300

    Update and rename questao05.java to questao06.java

--- questao06.java
+++ questao06.java
@@ -7,14 +7,15 @@ posteriormente, realize a soma de todos os algarismos deste mesmo número
 Grupo de programadores : Ana , Flavio e Matheus
 */
 
-import  java.util.Scanner; // Biblioteca Scanner
+import java.util.Scanner; // Importando a biblioteca Scanner
 
 public class questao05{
-
+    static Scanner sc = new Scanner(System.in); 
+    
     public static void main(String[] arg){
-        System.out.println("Informe um numero: "); //Solicitando numero ao usuario
-        int num = new Scanner(System.in).nextInt();
-        int soma = 0; // Varialvel responsavel para somar os algarismos separados . EX 12 = 1 + 2 = 3
+        System.out.println("Informe um numero: ");
+        int num = sc.nextInt(); // Variavel dos numeros
+        int soma = 0;
 
         while(num>0){
             // Obtenhe-se o modulo do numero antes de se realizar a soma
@@ -23,11 +24,11 @@ public class questao05{
              soma += modulo;
              // divide-se por fim por 10 para chegar no novo valor
              num/=10;
-             // procedimento sendo printado passo a passo na mudança 
-             System.out.println("Valor de soma é  ", +soma);
+             // procedimento sendo printado passo a passo
+             System.out.println("Valor de soma é  " + soma);
         }
         System.out.println(); // salto de linha
-        System.out.println("Valor total de todos os numeros digitados é ", +soma);//soma final
+        System.out.println("Valor total de todos os numeros digitados é " + soma); //soma final
 
 
     }
